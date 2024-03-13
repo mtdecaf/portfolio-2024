8f79af7fd5493bdc0dbfd99148a9e8452a8f80f1
Jacky Cao <jcvan0116@gmail.com>
2024-03-13 16:30:47 -0700

    tailwind functional

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="h-dvh center flex justify-center box-border p-24">
+      <div className="w-full max-w-7xl h-full  border-black border">
+
+      </div>
     </main>
   );
 }
